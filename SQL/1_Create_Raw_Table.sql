1c9f649b759263aac7b917c4b2035e01e69ef5a4
Y Baskara <yugo_baskara@yahoo.com>
2026-01-21 10:27:42 +0700

    Create 1_Create_Raw_Table.sql

---
+++ SQL/1_Create_Raw_Table.sql
@@ -0,0 +1,17 @@
+create table if not exists
+	portofolio.car_performance_raw
+(
+city_mpg int,
+class varchar (30),
+combination_mpg int,
+cylinders varchar (20),
+displacement varchar (20),
+drive varchar (20),
+fuel_type varchar (20),
+highway_mpg int,
+brand varchar (30),
+model varchar (100),
+transmission char (1),
+year int
+)
+;
